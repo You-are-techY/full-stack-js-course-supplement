06cba3d0844ebe7fc6d523f8397a5910bb75127d
Erik Guffrey <erik@fugitivelabs.com>
2020-07-29 17:52:58 -0400

    serve static advanced

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 3030;
+const serveStatic = require('serve-static');
+const path = require('path');
 
-app.use('/', express.static('public'));
+app.use('/', express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => console.log('send static directory'));
 
 
