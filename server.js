f29047ab14acbaf881e7172d76b36e61c9bcd2dd
Erik Guffrey <erik@fugitivelabs.com>
2020-08-05 18:21:47 -0400

    static files with express

--- server.js
+++ server.js
@@ -4,8 +4,17 @@ const app = express();
 const port = 3030;
 const hostname = 'localhost'; 
 
+/**
+ * This tells Express to look at the '/public' directory when we get a root get request
+ */
+// setup static files 
+app.use('/', express.static('public'));
+
 // setup a root GET request 
-app.get('/', (req, res) => res.send('Hello World! I am Express!!!'));
+app.get('/', (req, res) => {
+  console.log('serving static files');
+  res.send('Hello World! Express is serving static files!');
+});
 
 // setup the server 
 app.listen(port, () => console.log(`Example app listening at http://${hostname}:${port}`));
