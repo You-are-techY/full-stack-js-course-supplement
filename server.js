fd65a6ceada34df53e7c5203ec710cf30e75399e
Erik Guffrey <erik@fugitivelabs.com>
2020-08-05 18:16:11 -0400

    style

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 
 const port = 3030;
 const hostname = 'localhost'; 
 
 // setup a root GET request 
-app.get('/', (req, res) => res.send('Hello World! I am Express!!!'))
+app.get('/', (req, res) => res.send('Hello World! I am Express!!!'));
 
 // setup the server 
-app.listen(port, () => console.log(`Example app listening at http://${hostname}:${port}`))
+app.listen(port, () => console.log(`Example app listening at http://${hostname}:${port}`));
