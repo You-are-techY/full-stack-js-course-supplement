4b7d4b7fde20684b97fa26c469eaf46143ec4aae
Erik Guffrey <erik@fugitivelabs.com>
2020-08-05 18:10:40 -0400

    serve static files

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const http = require('http'); // this is part of Node
+const serveStatic = require('serve-static'); // npm install --save serve-static 
+const finalhandler = require('finalhandler'); // npm install --save finalhandler
 
 const port = 3030;
 const hostname = 'localhost'; 
 
+// Serve up public directory
+let serve = serveStatic('public', { 'index': ['index.html', 'index.htm'] })
  
 // Create server
 let server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  serve(req, res, finalhandler(req, res));
 })
  
 // Listen
