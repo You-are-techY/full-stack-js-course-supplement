c98b540b76a7c518b94c4a7ea29d151179d14b93
Erik Guffrey <erik@fugitivelabs.com>
2020-08-05 23:06:13 -0400

    setup getById

--- resources/todo/todosController.js
+++ resources/todo/todosController.js
@@ -5,15 +5,23 @@ exports.list = (req, res) => {
   Todo.find({}).exec((err, todos) => {
     if(err || !todos) {
       console.log("error!", err);
-      res.send({success: false, message: err});
+      res.send({ success: false, message: err });
     } else {
       console.log("Query successful");
-      res.send({success: true, todos});
+      res.send({ success: true, todos });
     }
   })
 }
 
 exports.getById = (req, res) => {
   console.log("fetch todo by _id", req.params.id);
-  Todo.find({_id: req.params.id}).exec(())
+  Todo.find({_id: req.params.id}).exec((err, todo) => {
+    if(err) {
+      res.send({ success: false, message: err });
+    } else if (!todo) {
+      res.send({ success: false, message: "Todo not found." });
+    } else {
+      res.send({ success: true, todo });
+    }
+  })
 }
