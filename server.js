6f73b280d32128cd34bd3f6f7d8673235db5402e
Erik Guffrey <erik@fugitivelabs.com>
2020-08-05 23:14:36 -0400

    CRUD routes

--- server.js
+++ server.js
@@ -14,8 +14,19 @@ app.get('/', (req, res) => res.send('Hello World!'));
 let todos = require('./resources/todo/todosController');
 
 // api routes 
+
+// - Create
+app.post('/api/todos', todos.create); 
+
+// - Read
 app.get('/api/todos', todos.list);
 app.get('/api/todos/:id', todos.getById);
+
+// - Update
+app.put('/api/todos/:id', todos.update); 
+
+// - Delete
+app.delete('/api/todos/:id', todos.delete); 
 
 
 // setup the server 
