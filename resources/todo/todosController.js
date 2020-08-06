f87828a7a858e2e7dcb89cdbd578d3c272f7e582
Erik Guffrey <erik@fugitivelabs.com>
2020-08-05 23:02:34 -0400

    add getById controller

--- resources/todo/todosController.js
+++ resources/todo/todosController.js
@@ -1,6 +1,7 @@
 let Todo = require('mongoose').model('Todo');
 
 exports.list = (req, res) => {
+  console.log("fetch all todos");
   Todo.find({}).exec((err, todos) => {
     if(err || !todos) {
       console.log("error!", err);
@@ -10,4 +11,9 @@ exports.list = (req, res) => {
       res.send({success: true, todos});
     }
   })
+}
+
+exports.getById = (req, res) => {
+  console.log("fetch todo by _id", req.params.id);
+  Todo.find({_id: req.params.id}).exec(())
 }
